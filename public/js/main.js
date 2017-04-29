8ebfe57fda558f0eccd0eb2cc8f1ca6712ece208
MikeStegall <mestegall5@gmail.com>
2017-04-29 14:57:30 -0500

    Added some functionality to the js code with button clicks and alerts.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,9 @@
 /* global $ */
 var loginURL = 'http://127.0.0.1:7979/api/login'
+var loginBtn = $('#loginBtn')
+function successfulLogin () {
+  window.location = 'http://127.0.0.1:7979/index.html'
+}
 
 function responseFail (error) {
   if (error === 500) {
@@ -7,8 +11,23 @@ function responseFail (error) {
   } else if (error === 400) {
     alert('Check your username and/or password!')
   }
+  loginBtn.attr('disable', false)
 }
 
-function functionName() {
-
+function waitingForServer () {
+  loginBtn.attr('disable', true)
 }
+
+function login () {
+  var username = $('#loginInput').val()
+  var password = $('#passwordInput').val()
+  if (username !== '' && password !== '') {
+    var data = { username: username, password: password }
+    var grabingAPI = $.post(loginURL, data).done().fail(responseFail)
+  } else {
+    alert('Please enter username and password')
+  }
+}
+
+
+loginBtn.click(login)
